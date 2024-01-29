2818fc8d123232249dfe9e30f78d6d62c94bc2fa
Prabal Arora <prabalarora2001@gmail.com>
2024-01-29 22:35:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import './App.css';
+import Header from './Components/header/Header';
+import Home from './Pages/Home/Home';
+import MovieList from './Components/MovieList/MovieList';
+import Movie from './Pages/movieDetail/movie';
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path='movie/:id' element={<Movie />}></Route>
+          <Route path='movies/:type' element={<MovieList />}></Route>
+          <Route path='/*' element={<h1>Error page</h1>}></Route>
+        </Routes>
+      </Router>
+      
+     </div>
+    </>
+  )
+}
+
+export default App
